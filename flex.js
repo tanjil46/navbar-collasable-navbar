3486adaf610eda8fac5880dab7fc114a429dc306
tanjil hoassain <tanjilt46@gmail.com>
2023-07-05 08:27:51 -0700

    collasble navbar

---
+++ flex.js
@@ -0,0 +1,7 @@
+ const barIcon=document.getElementById("my-icon");
+ const myNavbar=document.getElementById("nav-id");
+
+ barIcon.addEventListener("click",()=>{
+      myNavbar.classList.toggle("active");
+
+ })
